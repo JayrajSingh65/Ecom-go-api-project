913cdf3278546baee21c970bd71db3adcbcd97e4
JayrajSingh65 <singhjayraj69@gmail.com>
2025-12-02 19:36:37 +0530

    feat(api): add basic HTTP server setup with chi router

---
+++ cmd/api.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jayraj/myapp/internal/products"
+)
+
+//mount
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID) //important for rate limiting
+	r.Use(middleware.RealIP)    // important for rate limiting, analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("everthing is good for now"))
+	})
+
+	productHandler := products.NewHandler(nil)
+
+	r.Get("/products", productHandler.ListProducts)
+
+	return r
+
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:              app.config.addr,
+		Handler:           h,
+		ReadHeaderTimeout: time.Second * 30,
+		WriteTimeout:      time.Second * 10,
+		IdleTimeout:       time.Minute,
+	}
+
+	log.Printf("server is started at %s ", app.config.addr)
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+	//logger
+	//db driver
+}
+
+type config struct {
+	addr string
+	db   dbconfig
+}
+
+type dbconfig struct {
+	dns string //username - product etc..
+}
