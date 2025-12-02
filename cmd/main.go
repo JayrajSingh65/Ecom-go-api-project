964e65cb988b1cc40e74bf9d7667db75f60861d1
JayrajSingh65 <singhjayraj69@gmail.com>
2025-12-02 19:38:25 +0530

    chore(api): setup project structure and server bootstrap

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+func main() {
+	cfg := config{
+		addr: ":8080",
+		db:   dbconfig{},
+	}
+
+	api := application{
+		config: cfg,
+	}
+
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := api.run(api.mount()); err != nil {
+		slog.Error("server has failed to start", "error", err)
+		os.Exit(1)
+	}
+
+}
---
+++ internal/products/handler.go
@@ -0,0 +1,26 @@
+package products
+
+import (
+	"net/http"
+)
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *handler {
+	return &handler{
+		service: service,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+
+	//call the service ListProducts
+	// return the json in http response
+
+	products := struct {
+		Products []string `json:"products"`
+	}{}
+
+}
